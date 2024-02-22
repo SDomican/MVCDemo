f12a35d125a21dead70a7a42cace72702bfff234
Stephen Domican <stephen.p.domican@gmail.com>
2024-02-22 15:34:25 +0000

    Add project files.

---
+++ MVCDemoApp/Models/OrderDisplayModel.cs
@@ -0,0 +1,11 @@
+﻿using DataLibrary.Models;
+
+namespace MVCDemoApp.Models
+{
+    public class OrderDisplayModel
+    {
+        public OrderModel Order { get; set; }
+        public string ItemPurchased { get; set; }
+
+    }
+}
